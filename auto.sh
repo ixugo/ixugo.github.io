8a0fabbaf19cf5f57fd2407f36195eea87e9ea9b
xugo <xx@golang.space>
2023-02-14 13:08:01 +0800

    blog

--- auto.sh
+++ auto.sh
@@ -6,12 +6,13 @@ set -e
 rm -rf ./docs
 
 # 打包
-./hugo -D docs >> build.log
+./hugo -d docs >> build.log
 echo -e "\n打包完成\n"
 
 cp ./googlec03aeb90afb546ce.html ./docs
 echo "blog.golang.space" > ./docs/CNAME
 
+git add .
 git commit -m "blog" >> build.log
 echo -e "\n正在发送到服务器...\n"
 git push 
